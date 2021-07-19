daf94b3baf905d7a20723c9c2425cf6d0448ebd3
James Pederson <james@jpederson.com>
2021-07-19 10:56:44 -0500

    Add access control header to static template parts for SF.

--- footer-static.php
+++ footer-static.php
@@ -1,3 +1,4 @@
+<?php header( "Access-Control-Allow-Origin: *" ); ?>
 </div><!-- #content -->
 </section>
 <footer class="footer">
--- header-static.php
+++ header-static.php
@@ -1,3 +1,4 @@
+<?php header( "Access-Control-Allow-Origin: *" ); ?>
 <!DOCTYPE html>
 <!--[if IE 7]><html class="ie ie7" lang="en-US"><![endif]-->
 <!--[if IE 8]><html class="ie ie8" lang="en-US"><![endif]-->
