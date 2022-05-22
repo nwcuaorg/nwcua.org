81bcb0b91a2dab955facd767ee58430586e30e57
James Pederson <james@jpederson.com>
2022-05-21 21:18:25 -0500

    Add partner products section, and add more styles to the left column on partner pages.

--- single-partner.php
+++ single-partner.php
@@ -21,13 +21,17 @@ the_showcase();
 		<p>For more information on this solution please reach out to the Strategic Link team below.</p>
 		<p class="partner-buttons"><?php print do_shortcode( '[button url="mailto:strategic@nwcua.org" class="teal large"]Email Us[/button] [button url="tel:8009959064" class="teal large"]Call Us[/button]' ); ?></p>
 		<p>For more information on this solution please reach out to the Strategic Link team below.</p>
-		<p class="partner-social">
-			<?php print do_shortcode( '[button url="' . get_cmb_value( 'partner_website' ) . '" class="teal large"]Visit Website[/button]' ); ?>
-			<?php print partner_social_link( 'facebook' ); ?>
-			<?php print partner_social_link( 'twitter' ); ?>
-			<?php print partner_social_link( 'youtube' ); ?>
-			<?php print partner_social_link( 'linkedin' ); ?>
-		</p>
+		<div class="partner-links">
+			<div class="partner-website">
+				<?php print do_shortcode( '[button url="' . get_cmb_value( 'partner_website' ) . '" class="teal large"]Visit Website[/button]' ); ?>
+			</div>
+			<div class="partner-social">
+				<?php print partner_social_link( 'facebook' ); ?>
+				<?php print partner_social_link( 'twitter' ); ?>
+				<?php print partner_social_link( 'youtube' ); ?>
+				<?php print partner_social_link( 'linkedin' ); ?>
+			</div>
+		</div>
 	</div>
 	<div class="right-column">
 		<div class="right-column-inner">
@@ -39,8 +43,53 @@ the_showcase();
 				while ( have_posts() ) : the_post(); 
 					the_content();
 
+					// if we have a video
 					if ( has_cmb_value( 'partner_video' ) ) {
+						// output the video and process the oembed
 						print '<div class="partner-video">' . wp_oembed_get( get_cmb_value( 'partner_video' ) ) . '</div>';
+					}
+
+					// if we have products
+					if ( has_cmb_value( 'partner_products' ) ) {
+
+						// get the products
+						$products_string = get_cmb_value( 'partner_products' );
+
+						// get the products into an array
+						$products = explode( "\n", $products_string );
+
+						// count the products
+						$product_count = count( $products );
+
+						if ( !empty( $products ) ) {
+
+							// set counter
+							$c = 1;
+
+							// output the start of the products section
+							print '<h4 class="partner-products-title">Products Offered</h4><div class="partner-products"><div class="products-column"><ul>';
+
+							// loop through the products
+							foreach ( $products as $product ) {
+
+								// display product
+								print '<li>' . $product . '</li>';
+
+								// if we're halfway or more through the array, break to the second column.
+								if ( $c == round( $product_count/2 ) ) {
+									print '</ul></div><div class="products-column"><ul>';
+								}
+
+								// increment
+								$c++;
+
+							}
+
+							// close the products section
+							print '</ul></div></div>';
+
+						}
+
 					}
 				endwhile;
 			endif;
