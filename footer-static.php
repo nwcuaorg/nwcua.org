31593531b08a006142d16fa37746441ad6158598
James Pederson <james@jpederson.com>
2023-01-17 12:19:56 -0600

    Update static header and footer.

--- footer-static.php
+++ footer-static.php
@@ -33,30 +33,31 @@ SEATAC, WA 98188</p>
 </div>
 <div class="column">
 <h3>Links</h3>
-<div class="menu-footer-links-container"><ul id="menu-footer-links" class="menu"><li id="menu-item-92681" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92681"><a href="https://nwcua.org/about/">About Us</a></li>
+<div class="menu-footer-links-for-association-container"><ul id="menu-footer-links-for-association" class="menu"><li id="menu-item-92681" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92681"><a href="https://nwcua.org/about/">About Us</a></li>
 <li id="menu-item-93639" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-93639"><a href="https://nwcua.org/about/acknowledgement-of-risk/">Acknowledgement of Risk</a></li>
-<li id="menu-item-102338" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102338"><a href="https://nwcua.org/about/contact-us/">Contact the Association</a></li>
+<li id="menu-item-112439" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112439"><a href="https://gowestassociation.org/contact-us/">Contact the Association</a></li>
 <li id="menu-item-102336" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102336"><a href="https://nwcua.org/directory/">Credit Union Directory</a></li>
-<li id="menu-item-50" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-50"><a href="https://nwcuf.org/">Foundation</a></li>
+<li id="menu-item-50" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-50"><a href="https://nwcuf.org/">GoWest Foundation</a></li>
 <li id="menu-item-92689" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92689"><a href="https://nwcua.org/about/our-commitments/">Our Commitments</a></li>
 <li id="menu-item-93642" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-93642"><a href="https://nwcua.org/about/privacy-policy/">Privacy Policy</a></li>
-<li id="menu-item-51" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-51"><a href="https://nwcuastrategiclink.com/">Strategic Link</a></li>
-<li id="menu-item-102340" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102340"><a href="https://nwcua.org/anthem/submit-your-news/">Submit Your News</a></li>
-<li id="menu-item-102330" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102330"><a href="https://nwcua.org/subscribe-to-mailing-lists/">Subscribe To Us</a></li>
+<li id="menu-item-51" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-51"><a href="https://nwcua.org/solutions/">GoWest Solutions</a></li>
+<li id="menu-item-112441" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112441"><a href="https://gowestassociation.org/submit-your-news/">Submit Your News</a></li>
+<li id="menu-item-112440" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112440"><a href="https://gowestassociation.org/mailing-lists/">Subscribe To Us</a></li>
 <li id="menu-item-93636" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-93636"><a href="https://nwcua.org/trade-show-agreement/">Trade Show Agreement</a></li>
 </ul></div> </div>
 <div class="column">
 <h3>Resources</h3>
 <div class="menu-footer-resources-container"><ul id="menu-footer-resources" class="menu"><li id="menu-item-102328" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102328"><a href="https://nwcua.org/advocacy/">Advocacy</a></li>
-<li id="menu-item-92691" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92691"><a href="https://nwcua.org/anthem/">Anthem News</a></li>
+<li id="menu-item-112444" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112444"><a href="https://gowestassociation.org/onthego/">On The Go</a></li>
 <li id="menu-item-102333" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102333"><a href="https://nwcua.org/compliance/">Compliance</a></li>
 <li id="menu-item-92686" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-92686"><a href="/jobs/">Credit Union Careers</a></li>
 <li id="menu-item-102341" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-102341"><a href="https://secure.e2rm.com/p2p/event/336948">CU4Kids</a></li>
-<li id="menu-item-92693" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-92693"><a href="/events/">Events Calendar</a></li>
+<li id="menu-item-92693" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-92693"><a href="https://gowestassociation.org/events/">Events Calendar</a></li>
 <li id="menu-item-92687" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92687"><a href="https://nwcua.org/media-center/">Digital Media Center</a></li>
-<li id="menu-item-102331" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102331"><a href="https://nwcua.org/?page_id=92282">Safelist Us</a></li>
+<li id="menu-item-112445" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112445"><a href="https://gowestassociation.org/wp-content/uploads/2022/07/Safelisting-for-GoWest-Final.pdf">Safelist</a></li>
 <li id="menu-item-92690" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-92690"><a href="https://airtable.com/shrv5Rsr9m6eEbWud">Speak at a Future Event</a></li>
 <li id="menu-item-103687" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-103687"><a href="https://nwcua.org/travel-discounts/">Travel Information</a></li>
+<li id="menu-item-112446" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112446"><a href="https://gowestassociation.org/wp-content/uploads/2022/07/GoWest_FAQ.pdf">New Association FAQ</a></li>
 <li id="menu-item-102339" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102339"><a href="https://nwcua.org/about/website-faq/">Website FAQ</a></li>
 </ul></div> </div>
 <div class="column">
--- header-static.php
+++ header-static.php
@@ -29,25 +29,20 @@ gtag('config', 'UA-23488192-1');
 </div>
 <div class="account-tools">
 <a href="https://nwcua.force.com/s/redirect-with-url-params?url=https://nwcua.org/" class='account button'>Log In</a> </div>
+<!-- start nav update -->    
 <nav>
 <button class="menu-toggle">Show/hide Menu</button>
 <div class="menu-main-menu-container"><ul id="menu-main-menu" class="nav-menu"><li id="menu-item-101869" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-101869"><a href="#">Training &#038; Events</a>
 <ul class="sub-menu">
-<li id="menu-item-92647" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-92647"><a href="https://nwcua.org/awards/">Awards</a>
+<li id="menu-item-92647" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92647"><a href="https://nwcua.org/awards/">Awards</a></li>
+<li id="menu-item-114135" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-114135"><a href="https://gowestassociation.org/councilsb/">Councils</a>
 <ul class="sub-menu">
-<li id="menu-item-92648" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92648"><a href="https://nwcua.org/awards/national-cuna-awards/">National CUNA Awards</a></li>
-</ul>
-</li>
-<li id="menu-item-92650" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-92650"><a href="https://nwcua.org/councils/">Councils</a>
-<ul class="sub-menu">
-<li id="menu-item-93050" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-93050"><a href="https://nwcua.org/councils/">Learn More</a></li>
-<li id="menu-item-175" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-175"><a href="/events/?event_category=16">Meeting Dates</a></li>
-<li id="menu-item-176" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-176"><a href="/councils/#memberships">Memberships</a></li>
+<li id="menu-item-114136" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-114136"><a href="https://gowestassociation.org/councilsb/">Learn More</a></li>
 </ul>
 </li>
 <li id="menu-item-145" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-145"><a href="https://secure.e2rm.com/p2p/event/336948">CU4Kids</a>
 <ul class="sub-menu">
-<li id="menu-item-177" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-177"><a href="/event/maxx-annual-convention/#accordion-2">Annual Auction</a></li>
+<li id="menu-item-177" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-177"><a href="https://nwcua.org/event/maxx-annual-convention/#accordion-2">Annual Auction</a></li>
 <li id="menu-item-231" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-231"><a href="https://secure.e2rm.com/p2p/donation/336948">Donate</a></li>
 <li id="menu-item-178" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-178"><a href="https://secure.e2rm.com/p2p/event/336948">Learn More</a></li>
 </ul>
@@ -55,7 +50,6 @@ gtag('config', 'UA-23488192-1');
 <li id="menu-item-180" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-180"><a href="#">Education</a>
 <ul class="sub-menu">
 <li id="menu-item-108044" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-108044"><a href="https://nwcua.org/event/virtual-emerging-leaders/">Emerging Leaders</a></li>
-<li id="menu-item-181" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-181"><a href="https://cuphilosophyquiz.com/">Philosophy Manual &#038; Quiz</a></li>
 <li id="menu-item-182" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-182"><a href="https://cuwebtraining.com/a/nwcua/category/live">Webinars</a></li>
 </ul>
 </li>
@@ -63,11 +57,6 @@ gtag('config', 'UA-23488192-1');
 <ul class="sub-menu">
 <li id="menu-item-106638" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-106638"><a href="https://gowestassociation.org/events/">All Events Calendar</a></li>
 <li id="menu-item-106637" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-106637"><a href="https://nwcua.org/advocacy/advocacy-training-and-events/">Advocacy</a></li>
-<li id="menu-item-106636" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-106636"><a href="https://nwcua.org/compliance/compliance-training-and-events/">Compliance</a></li>
-<li id="menu-item-110046" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-110046"><a href="https://nwcua.org/event/ceo-exec-virtual-roundtable/">CEO &#038; Exec Roundtables</a></li>
-<li id="menu-item-104005" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-104005"><a href="https://nwcua.org/event/executives-retreat/">Executives@Skamania</a></li>
-<li id="menu-item-188" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-188"><a href="/event/maxx-annual-convention/">MAXX Annual Convention</a></li>
-<li id="menu-item-106763" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-106763"><a href="https://nwcua.org/event/strategic-link-exhibitor-trade-show/">MAXX Trade Show</a></li>
 <li id="menu-item-108468" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-108468"><a href="https://nwcua.org/seminars/">Seminars</a></li>
 </ul>
 </li>
@@ -78,7 +67,7 @@ gtag('config', 'UA-23488192-1');
 <ul class="sub-menu">
 <li id="menu-item-92652" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-92652"><a href="https://nwcua.org/advocacy/">Advocacy</a>
 <ul class="sub-menu">
-<li id="menu-item-195" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-195"><a href="/category/advocacy-on-the-move/">Blog</a></li>
+<li id="menu-item-195" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-195"><a href="https://nwcua.org/category/advocacy-on-the-move/">Blog</a></li>
 <li id="menu-item-92654" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92654"><a href="https://nwcua.org/advocacy/culac/">CULAC</a></li>
 <li id="menu-item-93630" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-93630"><a href="https://nwcua.org/advocacy/grassroots-advocacy-center/">Grassroots</a></li>
 <li id="menu-item-101940" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-101940"><a href="https://nwcua.org/category/advocacy/">News</a></li>
@@ -89,20 +78,19 @@ gtag('config', 'UA-23488192-1');
 <li id="menu-item-149" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-149"><a href="/compliance/">Compliance</a>
 <ul class="sub-menu">
 <li id="menu-item-92646" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92646"><a href="https://nwcua.org/compliance/bulletins-alerts/">Bulletins &#038; Alerts</a></li>
-<li id="menu-item-201" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-201"><a href="/events/?event_category=6522">Calendar</a></li>
+<li id="menu-item-201" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-201"><a href="https://nwcua.org/events/?event_category=6522">Calendar</a></li>
 <li id="menu-item-102274" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-102274"><a href="https://nwcua.org/category/compliance/compliance-news/">News</a></li>
 <li id="menu-item-101316" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-101316"><a href="https://nwcua.org/compliance/compliance-resources/">Resources</a></li>
 <li id="menu-item-101350" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-101350"><a href="https://nwcua.org/compliance/compliance-tools/">Tools</a></li>
-<li id="menu-item-106635" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-106635"><a href="https://nwcua.org/compliance/compliance-training-and-events/">Training &#038; Events</a></li>
 </ul>
 </li>
 <li id="menu-item-226" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-226"><a href="/jobs/">Credit Union Careers</a>
 <ul class="sub-menu">
 <li id="menu-item-92718" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92718"><a href="https://nwcua.org/job-manager/job-post/">Post a Job</a></li>
-<li id="menu-item-93631" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-93631"><a href="/jobs/">View Listings</a></li>
+<li id="menu-item-93631" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-93631"><a href="https://nwcua.org/jobs/">View Listings</a></li>
 </ul>
 </li>
-<li id="menu-item-92578" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-92578"><a href="/directory/">Credit Union Directory</a></li>
+<li id="menu-item-92578" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-92578"><a href="https://nwcua.org/directory/">Credit Union Directory</a></li>
 <li id="menu-item-102985" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102985"><a href="https://nwcua.org/dei-resources/">DEI Resource Library</a></li>
 <li id="menu-item-208" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-208"><a href="https://nwcuf.org">Foundation</a>
 <ul class="sub-menu">
@@ -114,12 +102,12 @@ gtag('config', 'UA-23488192-1');
 </li>
 <li id="menu-item-92753" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-92753"><a href="https://nwcua.org/micro-communities/">Micro-Communities</a>
 <ul class="sub-menu">
-<li id="menu-item-189" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-189"><a href="/resources/micro-communities/#accordion-1">Chapters</a></li>
-<li id="menu-item-190" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-190"><a href="/resources/micro-communities/#accordion-2">Global Womens Leadership Network</a></li>
+<li id="menu-item-189" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-189"><a href="https://nwcua.org/resources/micro-communities/#accordion-1">Chapters</a></li>
+<li id="menu-item-190" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-190"><a href="https://nwcua.org/resources/micro-communities/#accordion-2">Global Womens Leadership Network</a></li>
 <li id="menu-item-101394" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-101394"><a href="https://nwcua.org/micro-communities/micro-community-impact-celebration-and-awards/">Impact Awards</a></li>
 <li id="menu-item-92754" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92754"><a href="https://nwcua.org/micro-communities/juntos-avanzamos/">Juntos Avanzamos</a></li>
 <li id="menu-item-110869" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-110869"><a href="https://nwcua.org/micro-communities/northwest-cude-network/">NW CUDE Network</a></li>
-<li id="menu-item-192" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-192"><a href="/events/?event_category=6641">Upcoming Events</a></li>
+<li id="menu-item-192" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-192"><a href="https://nwcua.org/events/?event_category=6641">Upcoming Events</a></li>
 <li id="menu-item-92755" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-92755"><a href="https://nwcua.org/micro-communities/northwest-yp-network/">YP Network</a></li>
 </ul>
 </li>
@@ -142,9 +130,9 @@ gtag('config', 'UA-23488192-1');
 </li>
 <li id="menu-item-111728" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-111728"><a href="https://nwcua.org/solutions/">Solutions</a>
 <ul class="sub-menu">
-<li id="menu-item-218" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-218"><a href="https://nwcuastrategiclink.com/service-providers/">Business Partners</a></li>
-<li id="menu-item-219" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-219"><a href="https://nwcua.formstack.com/forms/strategic_link_partner_article_submission">Partner Article Submission</a></li>
-<li id="menu-item-111715" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-111715"><a href="https://nwcua.org/solutions/credit-union-resources/">Resources for Credit Unions</a></li>
+<li id="menu-item-112679" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112679"><a href="https://nwcua.org/solutions/partners/">Our Partners</a></li>
+<li id="menu-item-112685" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-112685"><a href="https://nwcua.org/solutions/partner-article-submission/">Submit Partner Article</a></li>
+<li id="menu-item-111715" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-111715"><a href="https://nwcua.org/solutions/credit-union-resources/">Resources for CU&#8217;s</a></li>
 <li id="menu-item-111716" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-111716"><a href="https://nwcua.org/solutions/partner-resources/">Resources for Partners</a></li>
 </ul>
 </li>
@@ -155,8 +143,8 @@ gtag('config', 'UA-23488192-1');
 <li id="menu-item-112280" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112280"><a href="https://gowestassociation.org/onthego/">On The Go</a></li>
 <li id="menu-item-92921" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-92921"><a href="https://nwcua.org/media-center/">Digital Media Center</a>
 <ul class="sub-menu">
-<li id="menu-item-93041" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-93041"><a href="/category/nwcua-in-the-news/">In the News</a></li>
-<li id="menu-item-157" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-157"><a href="/category/news-releases/">News Releases</a></li>
+<li id="menu-item-93041" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-93041"><a href="https://nwcua.org/category/nwcua-in-the-news/">In the News</a></li>
+<li id="menu-item-157" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-157"><a href="https://nwcua.org/category/news-releases/">News Releases</a></li>
 <li id="menu-item-102651" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-102651"><a href="https://nwcua.org/media-center/">Resources for Reporters</a></li>
 </ul>
 </li>
@@ -173,8 +161,8 @@ gtag('config', 'UA-23488192-1');
 <li id="menu-item-112359" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112359"><a href="https://gowestassociation.org/wp-content/uploads/2022/07/GoWest_FAQ.pdf">New Association FAQ</a></li>
 <li id="menu-item-92668" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-92668"><a href="https://nwcua.org/about/our-commitments/">Our Commitments</a>
 <ul class="sub-menu">
-<li id="menu-item-164" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-164"><a href="/our-commitments/#accordion-1">Code of Conduct</a></li>
-<li id="menu-item-163" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-163"><a href="/our-commitments/#accordion-2">Social Justice</a></li>
+<li id="menu-item-164" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-164"><a href="https://nwcua.org/our-commitments/#accordion-1">Code of Conduct</a></li>
+<li id="menu-item-163" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-163"><a href="https://nwcua.org/our-commitments/#accordion-2">Social Justice</a></li>
 </ul>
 </li>
 <li id="menu-item-112290" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112290"><a href="https://gowestassociation.org/wp-content/uploads/2022/07/Safelisting-for-GoWest-Final.pdf">Safelist Us</a></li>
